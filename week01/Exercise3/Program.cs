f9cc89ff597a24d1a119eb6975fa525df62d5a0d
OdafeOwhojedo <oowhojedo@byupathway.edu>
2025-07-03 22:08:13 +0100

    I wrote the codes for wk1 exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int magicNumber = 9;
+        Console.Write("What is the magic number?  ");
+        string userGuess = Console.ReadLine();
+        int guessNumber = int.Parse(userGuess);
+        if (guessNumber == magicNumber)
+        {
+            Console.Write($"Congratulations! you guessed the magic number {magicNumber}");
+
+        }
+        else if (guessNumber < magicNumber)
+        {
+            Console.Write("Make a higher guess.");
+        }
+        else
+        {
+            Console.Write("Make a lower guess");
+        }
     }
 }
