9052987a759cc29685a113980afc24ea92dcbdaf
OdafeOwhojedo <oowhojedo@byupathway.edu>
2025-07-03 21:01:04 +0100

    I completed wk1 exercise 2 assignment

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage?  ");
+        string grade = Console.ReadLine();
+        int userGrade = int.Parse(grade);
+        if (userGrade >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+            Console.Write("Congratulations! you pass");
+        }
+        else if (userGrade >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+            Console.Write("Congratulations! You pass");
+        }
+        else if (userGrade >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+            Console.Write("Congratulations! You pass");
+        }
+        else if (userGrade >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+            Console.Write("Congratulations! You passed");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+            Console.Write("Sorry, you failed the course, please try again.");
+        }
+
+        
     }
 }
