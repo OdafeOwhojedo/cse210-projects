932815dd11904fde470c8a672d159884149fb1fe
OdafeOwhojedo <oowhojedo@byupathway.edu>
2025-07-03 23:07:09 +0100

    I updated this code using a random generator to get the magic number to be guessed by the user.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,22 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        int magicNumber = 9;
-        Console.Write("What is the magic number?  ");
-        string userGuess = Console.ReadLine();
-        int guessNumber = int.Parse(userGuess);
-        if (guessNumber == magicNumber)
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        int guessedNumber = 0;
+        while (guessedNumber != magicNumber)
         {
-            Console.Write($"Congratulations! you guessed the magic number {magicNumber}");
-
+            Console.Write("What is the magic number?  ");
+            guessedNumber = int.Parse(Console.ReadLine());
+            if (guessedNumber < magicNumber)
+            {
+                Console.Write("Please make a higher guess ");
+            }
+            else if (guessedNumber > magicNumber)
+            {
+                Console.Write("Please make a lower guess ");
+            }
+            else
+            {
+            Console.WriteLine($"Congratulations! you guessed the magic number {magicNumber}");
+            }
         }
-        else if (guessNumber < magicNumber)
-        {
-            Console.Write("Make a higher guess.");
-        }
-        else
-        {
-            Console.Write("Make a lower guess");
-        }
+        
     }
 }
