d3776ed41b0bfa156a1aa6579c16937c1c1a30cc
OdafeOwhojedo <oowhojedo@byupathway.edu>
2025-07-08 22:08:12 +0100

    Made changes to the WriteLine

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,23 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        Console.WriteLine("What is your job title?");
+        Console.Write("What is your job title?  ");
         job1._jobTitle = Console.ReadLine();
-        Console.WriteLine("What is your company name?");
+        Console.Write("What is your company name?  ");
         job1._companyName = Console.ReadLine();
-        Console.WriteLine("Enter the start year.");
+        Console.Write("Enter the start year.  ");
         job1._startYear = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter the end year.");
+        Console.Write("Enter the end year.  ");
         job1._endYear = int.Parse(Console.ReadLine());
         job1.DisplayJobInfor();
         Job job2 = new Job();
-        Console.WriteLine("What is your job title?");
+        Console.Write("What is your job title?  ");
         job2._jobTitle = Console.ReadLine();
-        Console.WriteLine("What is your company name?");
+        Console.Write("What is your company name?  ");
         job2._companyName = Console.ReadLine();
-        Console.WriteLine("Enter the start year.");
+        Console.Write("Enter the start year.  ");
         job2._startYear = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter the end year.");
+        Console.Write("Enter the end year.  ");
         job2._endYear = int.Parse(Console.ReadLine());
         job2.DisplayJobInfor();
 
