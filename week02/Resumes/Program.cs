164675d0f78cac3e576ac391a2c8f0b8a82337ee
OdafeOwhojedo <oowhojedo@byupathway.edu>
2025-07-08 21:50:54 +0100

    I added the display function

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        Console.WriteLine("What is your job title?");
+        job1._jobTitle = Console.ReadLine();
+        Console.WriteLine("What is your company name?");
+        job1._companyName = Console.ReadLine();
+        Console.WriteLine("Enter the start year.");
+        job1._startYear = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the end year.");
+        job1._endYear = int.Parse(Console.ReadLine());
+        job1.DisplayJobInfor();
     }
 }
