c3c839d617a9afa8bc9d5c4c88bfb24c4cf7a291
OdafeOwhojedo <oowhojedo@byupathway.edu>
2025-09-05 18:52:42 +0100

    I have completed the activity for this wk

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction firstFraction = new Fraction();
+        string displayStringResult;
+        double displayDecimalResult;
+        displayStringResult = firstFraction.GetFraction();
+        displayDecimalResult = firstFraction.GetDecimalValue();
+        Console.WriteLine(displayStringResult);
+        Console.WriteLine(displayDecimalResult);
+        int userNumber;
+        Console.Write("Enter the numerator of your fraction:   ");
+        userNumber = int.Parse(Console.ReadLine());
+        Fraction secondFraction = new Fraction(userNumber);
+        displayStringResult = secondFraction.GetFraction();
+        displayDecimalResult = secondFraction.GetDecimalValue();
+        Console.WriteLine(displayStringResult);
+        Console.WriteLine(displayDecimalResult);
+        int userNumerator;
+        int userDenominator;
+        Console.Write("Enter any number for the numerator other than zero(0) for your fraction:   ");
+        userNumerator = int.Parse(Console.ReadLine());
+        Console.Write("Enter any number for the denominator other than zero(0) for your fraction:   ");
+        userDenominator = int.Parse(Console.ReadLine());
+        Fraction thirdFraction = new Fraction(userNumerator, userDenominator);
+        displayStringResult = thirdFraction.GetFraction();
+        displayDecimalResult = thirdFraction.GetDecimalValue();
+        Console.WriteLine(displayStringResult);
+        Console.WriteLine(displayDecimalResult);
+        Console.Write("Enter any number for the numerator except zero(0) for your fraction:   ");
+        userNumerator = int.Parse(Console.ReadLine());
+        Console.Write("Enter any number for the denominator except zero(0) for your fraction:   ");
+        userDenominator = int.Parse(Console.ReadLine());
+        Fraction fourthFraction = new Fraction(userNumerator, userDenominator);
+        fourthFraction.SetTop(userNumerator);
+        fourthFraction.SetBottom(userDenominator);
+        displayStringResult = fourthFraction.GetFraction();
+        displayDecimalResult = fourthFraction.GetDecimalValue();
+        Console.WriteLine(displayStringResult);
+        Console.WriteLine(displayDecimalResult);
+        int confirmNumerator;
+        int confirmDenominator;
+        confirmNumerator = fourthFraction.GetTop();
+        Console.WriteLine($"The numerator of the fraction is:  {confirmNumerator}");
+        confirmDenominator = fourthFraction.GetBottom();
+        Console.WriteLine($"The denominator of the fraction is:  {confirmDenominator}");
+
     }
 }
---
+++ week03/Fractions/fraction.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    public string GetFraction()
+    {
+        return _top.ToString() + "/" + _bottom.ToString();
+    }
+    public double GetDecimalValue()
+    {
+        double result;
+        result = (double) _top /(double) _bottom;
+        return result;
+    }
+
+}
