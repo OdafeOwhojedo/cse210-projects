203e7a8fa7cb73499c5355744ab8eae5fa5a6453
OdafeOwhojedo <oowhojedo@byupathway.edu>
2025-09-26 00:11:15 +0100

    change a variable

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -1,19 +1,13 @@
 using System;
 public class Comment
 {
-   // public int Id;
-   // public int VideoId;
     public string _nameOfCommenter;
     public string _commentText;
-   // public DateTime Timestamp;
 
     public Comment(string nameOfCommenter, string commentText)
     {
-        // Id = id;
-        //VideoId = viewedVideoId;
         _nameOfCommenter = nameOfCommenter;
         _commentText = commentText;
-        //Timestamp = DateTime.Now;
     }
 
     public string DisplayCommentDetails()
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 
 public class Video
 {
-    //public int Id;
     public string _videoTitle;
     public string _authorOfVideo;
     public int _lengthInSeconds;
-    public Comment myComment;
+    public Comment _userComment;
     public List<Comment> _comments;
 
     public Video(string videoTitle, string authorOfVideo, int lengthInSeconds )
     {
-        // Id = id;
         _videoTitle = videoTitle;
         _authorOfVideo = authorOfVideo;
         _lengthInSeconds = lengthInSeconds;
@@ -30,23 +28,5 @@ public class Video
         return numberOfComments;
     }
 
-    //public void DisplayVideoDetails()
-    //{
-       // Console.WriteLine($"------- Video: {Title} (ID: {Id}) ------");
-       // Console.WriteLine($"Duration: {DurationInSeconds} second");
-        //Console.WriteLine("\nComments:");
-        //if (Comments.Count == 0)
-        //{
-           // Console.WriteLine(" No comments");
-       // }
-       // else
-       // {
-           // foreach (var comment in Comments)
-            //{
-               // comment.DisplayCommentDetails();
-           // }
-        //}
-       // Console.WriteLine("------------------");
-   // }
 
 }
